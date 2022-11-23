c0dbb51c28de96e6192776c352a58aa19950e0c1
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-23 18:36:19 +0100

    Modified TextureSound script

--- TextureSound.cs
+++ TextureSound.cs
@@ -1,13 +1,10 @@
 ﻿using UnityEngine;
-using System.Collections;
 using System;
 
 [Serializable]
 public class TextureSound {
 
-    //Takstura z która zostanie powiązany dzwięk.
     public Texture texture;
-    //Dzwięk który będzie odtwarzany.
     public AudioClip sound;
     	
 }
