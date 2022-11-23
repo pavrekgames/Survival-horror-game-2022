c5da08cd77752be26446e807bb18151539076044
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-23 09:49:09 +0100

    Refactored CrouchCollison script

--- CrouchCollision.cs
+++ CrouchCollision.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class CrouchCollision : MonoBehaviour {
 
-	public float upMargin = 0.1f;
-	public Transform player;
 	public bool isCollide = false;
-	
-
-	void Update () {
-
-		//transform.position = new Vector3(Gracz.transform.position.x, Gracz.transform.position.y + ((Gracz.GetComponent<CharacterController>().height * Gracz.localScale.y) / 2) + (transform.localScale.y / 2),Gracz.transform.position.z) ;
-		//transform.position.z = Gracz.transform.position.z;
-		//transform.position.y = Gracz.transform.position.y + ((Gracz.GetComponent<Collider>().height * Gracz.localScale.y) / 2) + (transform.localScale.y / 2) + MiejsceNad;
-
-	}
 
 	void OnCollisionStay(Collision col){
 
-		if(col.transform.tag == "KucanieKolizja"){
+		if(col.transform.tag == "CrouchCollision"){
 			isCollide = true;
 		}
 	}
