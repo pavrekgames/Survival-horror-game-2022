fce2854efd9d01265ce892ab6d5aee8dba388d33
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-25 11:53:47 +0100

    Refactored picking up items in Inventory script

--- Inventory/Item.cs
+++ Inventory/Item.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName ="Item", menuName ="Item")]
 [System.Serializable]
-public class Item{
+public class Item: ScriptableObject{
 
 	public int id;
     public string type;
@@ -11,12 +12,14 @@ public class Item{
 	public string description;
     public Sprite icon;
 	public bool isUsed;
+    public bool isTaken;
+    public bool isRemoved;
 
 	public Item(){
 		
 	}
 
-	public Item(int id, string type, string name, string description, Sprite icon, bool isUsed){
+	public Item(int id, string type, string name, string description, Sprite icon, bool isUsed, bool isTaken, bool isRemoved){
 
 		this.id = id;
         this.type = type;
@@ -24,6 +27,8 @@ public class Item{
 		this.description = description;
         this.icon = icon;
 		this.isUsed = isUsed;
+        this.isTaken = isTaken;
+        this.isRemoved = isRemoved;
 
 	}
 }
