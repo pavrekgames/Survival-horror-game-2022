d3e208061cac1b604ecee8df0a052f0fca215172
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-28 12:08:55 +0100

    Modified tasks script and created RaycastTask interface

---
+++ Tasks/Raycast Tasks/UnlockDoor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnlockDoor : MonoBehaviour, IRaycastTask {
+
+    private Inventory inventoryScript;
+    private OpenCloseObject openCloseObjectsScript;
+    public AudioSource audioSource;
+    public AudioClip keyOpenSound;
+    public GameObject doorCollider;
+    public GameObject additionalObject;
+    public Door doorScript;
+    public bool isLocked;
+    public string itemType;
+
+    void Start()
+    {
+        inventoryScript = GameObject.Find("Player").GetComponent<Inventory>();
+        openCloseObjectsScript = GameObject.Find("Player").GetComponent<OpenCloseObject>();
+    }
+
+    public void Execute()
+    {
+        for (int i = 0; i < inventoryScript.items.Count; i++)
+        {
+            if (inventoryScript.items[i].type == itemType && inventoryScript.items[i].isUsed == true)
+            {
+                openCloseObjectsScript.enabled = false;
+                audioSource.clip = keyOpenSound;
+                audioSource.Play();
+                doorCollider.gameObject.SetActive(false);
+                isLocked = false;
+                doorScript.isNeedKey = false;
+                openCloseObjectsScript.enabled = true;
+
+                if(additionalObject != null)
+                {
+                    additionalObject.SetActive(false);
+                }
+
+                inventoryScript.RemoveItem(inventoryScript.items[i], true);
+                break;
+            }
+        }
+    }
+
+}
