63f7a788d478d14701bc53543e2a4c49ea2bd68a
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-29 11:04:32 +0100

    Modiefied tasks scripts

--- Tasks/TaskData.cs
+++ Tasks/TaskData.cs
@@ -1,15 +1,21 @@
-﻿
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName ="Task", menuName ="Task")]
 [System.Serializable]
-public class TaskData {
+public class TaskData : ScriptableObject {
 
     public string title;
     public string content;
+    public bool isAdded;
+    public bool isRemoved;
 
-    public TaskData(string title, string content)
+    public TaskData(string title, string content, bool isAdded, bool isRemoved)
     {
 
         this.title = title;
         this.content = content;
+        this.isAdded = isAdded;
+        this.isRemoved = isRemoved;
 
     }
 
