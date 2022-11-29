3bcc7ece00b63229ba01c5630111623e65abb8a7
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-29 10:02:45 +0100

    Screamer scripts refactored

--- Screamers/ScreamerDeadShock.cs
+++ Screamers/ScreamerDeadShock.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScreamerDeadShock : MonoBehaviour {
+public class ScreamerDeadShock : MonoBehaviour, IScreamer {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [SerializeField] private AudioSource shockAudioSource;
+    [SerializeField] private AudioClip shockSound;
+    [SerializeField] private AudioSource suffocateAudioSource;
+    [SerializeField] private AudioClip suffocateSound;
+    [SerializeField] private Animator deadAnimator;
+
+    public bool isCalled;
+
+    public void CallScreamer()
+    {
+        shockAudioSource.clip = shockSound;
+        shockAudioSource.Play();
+        suffocateAudioSource.clip = suffocateSound;
+        suffocateAudioSource.Play();
+        isCalled = true;
+        deadAnimator.SetTrigger("Shock");
+    }
 }
