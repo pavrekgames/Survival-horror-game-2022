b4e90da8f139c943dc5354d5d9c06d5e2ccf3c88
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-11-30 18:20:28 +0100

    Refactored VoiceActing scripts

---
+++ Screamers/ScreamerSoundWithEvent.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreamerSoundWithEvent : MonoBehaviour {
+
+    public event Action OnCallScreamer;
+
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip screamerSound;
+    public bool isCalled;
+
+    public void CallScreamer()
+    {
+        audioSource.clip = screamerSound;
+        audioSource.Play();
+        audioSource.loop = false;
+        isCalled = true;
+
+        if(OnCallScreamer != null)
+        {
+            OnCallScreamer.Invoke();
+        }
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") && isCalled == false)
+        {
+            CallScreamer();
+        }
+    }
+}
---
+++ VoiceTrigger.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceTrigger : MonoBehaviour {
+
+    private VoiceActing voiceActingScript;
+
+    private AudioSource audioSource;
+    private AudioClip recording;
+
+    public bool isVoice;
+
+    public enum VoiceType
+    {
+        Voice,
+        VoiceRetro
+    }
+
+    public VoiceType voiceType;
+
+    void Start()
+    {
+        voiceActingScript = GameObject.Find("Player").GetComponent<VoiceActing>();
+    }
+	
+    void OnTriggerEnter(Collider other)
+    {
+        if(isVoice == false)
+        {
+            if (other.gameObject.CompareTag("Player") && voiceType == VoiceType.Voice)
+            {
+                voiceActingScript.PlayVoice(audioSource, recording);
+                isVoice = true;
+            }
+            else if(other.gameObject.CompareTag("Player") && voiceType == VoiceType.VoiceRetro)
+            {
+                voiceActingScript.PlayRetrospection(recording);
+                isVoice = true;
+            }
+        }
+
+       
+    }
+
+}
