b5e724aa9f79fbfea746f5c9b8d5e181477f6051
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-12-02 14:03:22 +0100

    Refactored Monsters scripts

---
+++ Monsters/Monster.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public abstract class Monster : MonoBehaviour {
+
+    protected Transform player;
+    protected NavMeshAgent monsterAgent;
+    protected CharacterController monsterController;
+    protected Flashlight flashlightScript;
+    protected Health healthScript;
+    protected Crouch crouchScript;
+    protected Jumpscare jumpscareScript;
+    protected Map mapScript;
+    protected Ray monsterAim;
+
+    [Header("Monster components")]
+    [SerializeField] protected Transform playerHead;
+    [SerializeField] protected Transform monster;
+    [SerializeField] protected Animator monsterAnimator;
+    [SerializeField] protected AudioSource audioSource;
+    [SerializeField] protected AudioClip monsterSound;
+    [SerializeField] protected AudioClip monsterAttackSound;
+
+    [Header("Monster state")]
+    [SerializeField] protected bool isSawPlayer = false;
+    [SerializeField] protected bool isRayPlayer = false;
+    [SerializeField] protected bool isSawLight = false;
+    [SerializeField] protected bool isAttack = false;
+
+    [Header("Monster movement")]
+    [SerializeField] protected float walkVelocity = 5f;
+    [SerializeField] protected float runVelocity = 7f;
+    [SerializeField] protected float currentVelocity = 4.0f;
+    [SerializeField] protected float rotationVelocity = 5.0f;
+    [SerializeField] protected float height = 0f;
+
+    [Header("Monster ray")]
+    [SerializeField] protected float rayLength = 30f;
+
+    public abstract void MonsterFollowsPoint();
+    public abstract void MonsterFollowsPlayer(float _distance);
+    public abstract void MonsterLongLight(float _distance);
+    public abstract void MonsterFlashlight(float _distance);
+    public abstract void MonsterRaycast(float _distance);
+    public abstract void MonsterAttack(float _distance);
+
+}
