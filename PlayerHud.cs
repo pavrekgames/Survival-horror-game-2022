0d735bc1fd2a4bc2a5ea34219836a729df7d23da
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-12-04 20:58:33 +0100

    Modified scripts

---
+++ PlayerHud.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerHud : MonoBehaviour {
+
+    [SerializeField] private Canvas hudCanvas;
+    [SerializeField] private TextMeshProUGUI currenntItemTitle;
+    [SerializeField] private Image currentItemIcon;
+    private string currentItemText = "Item";
+
+    void Start () {
+		
+	}
+	
+
+	void Update () {
+
+        if (Time.timeScale == 0)
+        {
+            hudCanvas.enabled = false;
+        }
+        else if (Time.timeScale == 1)
+        {
+            hudCanvas.enabled = true;
+        }
+
+    }
+
+    void UpdateHud()
+    {
+        currentItemIcon.sprite = null;
+        currentItemIcon.color = Color.black;
+        currenntItemTitle.text = currentItemText;
+    }
+}
