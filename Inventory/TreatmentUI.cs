cae9c8d4c8a817aed8e88c0242063ffe7cb14145
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-12-05 19:53:49 +0100

    Refactored  TreatmentUI script

--- Inventory/TreatmentUI.cs
+++ Inventory/TreatmentUI.cs
@@ -6,132 +6,44 @@ using UnityEngine.UI;
 
 public class TreatmentUI : MonoBehaviour {
 
-    // inventory UI
+    public static bool isTreatmentActive = false;
 
-    public TextMeshProUGUI itemDescriptionText;
-    public TextMeshProUGUI usedItemText;
-    public Text secretItemsText;
-    public Text secretPlacesText;
-    public AudioClip menuButtonSound;
-    public AudioClip useItemSound;
-    public AudioClip itemDesciptionSound;
+    private Inventory inventoryScript;
     private Player playerScript;
     private CrosshairGUI cursorScript;
-    private AudioSource itemAudioSource3;
-    private Canvas inventoryCanvas;
-    public string usingItemText = " is using now!";
+    private Health healthScript;
 
-    public Image[] itemIcons;
-    private PlayerManager playerManagerScript;
-    public List<Item> items = new List<Item>();
-    private Animator animator;
-    private Transform player;
-    private Map mapScript;
-    private BeginGame beginGameScript;
-    public Flashlight flashlightScript;
-    private SaveGame SaveGameScript;
-    private Canvas uiCanvas;
-    public TextMeshProUGUI currenntItemTitle;
-    public Image currentItemIcon;
-    public bool isInventoryActive = false;
-    private Menu gameMenuScript;
-    public Notifications notificationScript;
-    public Tasks tasksScript;
-    public TaskBones bonesTaskScript;
-    public AudioSource itemAudioSource1;
-    public AudioSource itemAudioSource2;
-    public AudioSource itemAudioSource4;
-    public AudioSource pauseAudioSource;
-    public Health healthScript;
-    public Notes notesScript;
-    public VoiceActing voiceActingScript;
+    [Header("Audio")]
+    [SerializeField] private AudioSource itemAudioSource1;
+    [SerializeField] private AudioSource itemAudioSource2;
+    [SerializeField] private AudioSource itemAudioSource3;
+    [SerializeField] private AudioSource pauseAudioSource;
+    [SerializeField] private AudioClip menuButtonSound;
+    [SerializeField] private AudioClip itemDesciptionSound;
+    [SerializeField] private AudioClip openInventorySound;
+    [SerializeField] private AudioClip createPotSound;
+    [SerializeField] private AudioClip usePotSound;
+    [SerializeField] private AudioClip lackVialsSound;
 
-    public int secretItemsCount = 0;
-    public int secretPlacesCount = 0;
-    public int blueHerbsCount = 0;
-    public int greenHerbsCount = 0;
-    public int healthPotsCount = 0;
-    public int staminaPotsCount = 0;
-    public int vialsCount = 0;
+    [Header("UI")]
+    [SerializeField] private Canvas treatmentCanvas;
+    [SerializeField] private TextMeshProUGUI mixturesText;
+    [SerializeField] private TextMeshProUGUI vialsText;
+    [SerializeField] private Text blueHerbsText;
+    [SerializeField] private Text greenHerbsText;
+    [SerializeField] private Text vialsCountText;
+    [SerializeField] private Text healthPotsText;
+    [SerializeField] private Text staminaPotsText;
+    [SerializeField] private TextMeshProUGUI healthConditionText;
 
-    public AudioClip secretItemSound;
-    public AudioClip secretItemSound2;
-    public AudioClip secretPlaceSound;
-    public AudioClip collectHerbSound;
-    public AudioClip collectVialSound;
-    public AudioClip collectItemSound;
-    public AudioClip openInventorySound;
-
-    // tasks UI
-
-    public Canvas tasksCanvas;
-    public bool isTasksActive = false;
-    // notes UI
-
-    public Canvas notesCanvas;
-    public Canvas noteDefaultCanvas;
-    public bool isNotesActive = false;
-
-    private ScrollRect notesScrollRect;
-    private Scrollbar notesScrollbar;
-
-    // treatment
-
-    // treatment UI
-
-    public Canvas treatmentCanvas;
-    public TextMeshProUGUI mixturesText;
-    public TextMeshProUGUI vialsText;
-    public Text blueHerbsText;
-    public Text greenHerbsText;
-    public Text vialsCountText;
-    public Text healthPotsText;
-    public Text staminaPotsText;
-    private TextMeshProUGUI healthConditionText;
-    public static bool isTreatmentActive = false;
-    public AudioClip createPotSound;
-    public AudioClip usePotSound;
-    public AudioClip lackVialsSound;
-
-    // collection badges UI
-
-    public Canvas badgeCollectionCanvas;
-    public Canvas[] collectionCanvas;
-    private TextMeshProUGUI badgeCollectionTitleText;
-    public bool isCollectionActive = false;
-
-    public Sprite badgeSprite;
-    public Sprite badgeOKSprite;
-    public Image[] collectionTextures;
-
-    // collection photos
-
-
-    // collection photos UI
-
-    public Canvas photoCollectionCanvas;
-    private TextMeshProUGUI photoCollectionTitleText;
-    public Sprite photoSprite;
-    public Sprite photoOKSprite;
-
-    // collection tips
-
-
-    // collection tips UI
-
-    public Canvas tipCollectionCanvas;
-    private TextMeshProUGUI tipCollectionTitleText;
-    public Sprite tipSprite;
-    public Sprite tipOKSprite;
-
-    public string defaultPotDescription = "Hover over the mixture to get more information.";
-    public string healthPotDescription = "Health Mixture  - Increase your health. You need 2 green herbs and 2 blue herbs to create it.";
-    public string staminaPotDescription = "Stamina Mixture  - Increase your Stamina. You need 1 green herb and 2 blue herbs to create it.";
-    public string stateGoodText = "<color=#08FF5BFF>Good</color>";
-    public string stateInjuredText = "<color=#FFC117FF>Injured</color>";
-    public string stateCriticalText = "<color=#FF4E26FF>Critical</color>";
-    public string stateTiredText = "<color=#BF42C7FF>Tired</color>";
-    public string lackComponentsText = "<color=#FF0000FF>You don't have enough herbs or a vial</color>";
+    [Header("Texts")]
+    private string healthPotDescription = "Health Mixture  - Increase your health. You need 2 green herbs and 2 blue herbs to create it.";
+    private string staminaPotDescription = "Stamina Mixture  - Increase your Stamina. You need 1 green herb and 2 blue herbs to create it.";
+    private string stateGoodText = "<color=#08FF5BFF>Good</color>";
+    private string stateInjuredText = "<color=#FFC117FF>Injured</color>";
+    private string stateCriticalText = "<color=#FF4E26FF>Critical</color>";
+    private string stateTiredText = "<color=#BF42C7FF>Tired</color>";
+    private string lackComponentsText = "<color=#FF0000FF>You don't have enough herbs or a vial</color>";
 
     void Update()
     {
@@ -145,7 +57,7 @@ public class TreatmentUI : MonoBehaviour {
     {
         InventoryUIManager.ResetUI();
         CheckHealthCondition();
-        itemAudioSource3.PlayOneShot(menuButtonSound);
+        itemAudioSource2.PlayOneShot(menuButtonSound);
         treatmentCanvas.enabled = true;
         isTreatmentActive = true;
     }
@@ -166,34 +78,34 @@ public class TreatmentUI : MonoBehaviour {
 
     public void ShowHealthPotDescription()
     {
-        itemAudioSource3.PlayOneShot(itemDesciptionSound);
+        itemAudioSource2.PlayOneShot(itemDesciptionSound);
         mixturesText.text = healthPotDescription;
     }
 
     public void ShowStaminaPotDescription()
     {
-        itemAudioSource3.PlayOneShot(itemDesciptionSound);
+        itemAudioSource2.PlayOneShot(itemDesciptionSound);
         mixturesText.text = staminaPotDescription;
     }
 
     public void CreateHealthPot()
     {
 
-        if (greenHerbsCount >= 2 && blueHerbsCount >= 2 && vialsCount > 0)
+        if (inventoryScript.greenHerbsCount >= 2 && inventoryScript.blueHerbsCount >= 2 && inventoryScript.vialsCount > 0)
         {
-            greenHerbsCount -= 2;
-            blueHerbsCount -= 2;
-            vialsCount--;
-            healthPotsCount++;
+            inventoryScript.greenHerbsCount -= 2;
+            inventoryScript.blueHerbsCount -= 2;
+            inventoryScript.vialsCount--;
+            inventoryScript.healthPotsCount++;
             itemAudioSource1.PlayOneShot(createPotSound);
-            healthPotsText.text = healthPotsCount + "";
-            greenHerbsText.text = greenHerbsCount + "";
-            blueHerbsText.text = blueHerbsCount + "";
-            vialsCountText.text = vialsCount + "";
+            healthPotsText.text = inventoryScript.healthPotsCount.ToString();
+            greenHerbsText.text = inventoryScript.greenHerbsCount.ToString();
+            blueHerbsText.text = inventoryScript.blueHerbsCount.ToString();
+            vialsCountText.text = inventoryScript.vialsCount.ToString();
         }
         else
         {
-            itemAudioSource3.PlayOneShot(lackVialsSound);
+            itemAudioSource2.PlayOneShot(lackVialsSound);
             mixturesText.text = lackComponentsText;
         }
     }
@@ -201,21 +113,21 @@ public class TreatmentUI : MonoBehaviour {
     public void CreateStaminaPot()
     {
 
-        if (greenHerbsCount >= 1 && blueHerbsCount >= 2 && vialsCount > 0)
+        if (inventoryScript.greenHerbsCount >= 1 && inventoryScript.blueHerbsCount >= 2 && inventoryScript.vialsCount > 0)
         {
-            greenHerbsCount -= 1;
-            blueHerbsCount -= 2;
-            vialsCount--;
-            staminaPotsCount++;
-            itemAudioSource3.PlayOneShot(createPotSound);
-            staminaPotsText.text = staminaPotsCount + "";
-            greenHerbsText.text = greenHerbsCount + "";
-            blueHerbsText.text = blueHerbsCount + "";
-            vialsCountText.text = vialsCount + "";
+            inventoryScript.greenHerbsCount -= 1;
+            inventoryScript.blueHerbsCount -= 2;
+            inventoryScript.vialsCount--;
+            inventoryScript.staminaPotsCount++;
+            itemAudioSource2.PlayOneShot(createPotSound);
+            staminaPotsText.text = inventoryScript.staminaPotsCount.ToString();
+            greenHerbsText.text = inventoryScript.greenHerbsCount.ToString();
+            blueHerbsText.text = inventoryScript.blueHerbsCount.ToString();
+            vialsCountText.text = inventoryScript.vialsCount.ToString();
         }
         else
         {
-            itemAudioSource3.PlayOneShot(lackVialsSound);
+            itemAudioSource2.PlayOneShot(lackVialsSound);
             mixturesText.text = lackComponentsText;
         }
     }
@@ -223,24 +135,24 @@ public class TreatmentUI : MonoBehaviour {
     public void UseHealthPot()
     {
 
-        if (healthPotsCount > 0)
+        if (inventoryScript.healthPotsCount > 0)
         {
-            healthPotsCount--;
+            inventoryScript.healthPotsCount--;
             healthScript.health += 50;
-            itemAudioSource4.PlayOneShot(usePotSound);
-            healthPotsText.text = healthPotsCount + "";
+            itemAudioSource3.PlayOneShot(usePotSound);
+            healthPotsText.text = inventoryScript.healthPotsCount.ToString();
         }
     }
 
     public void UseStaminaPot()
     {
 
-        if (staminaPotsCount > 0)
+        if (inventoryScript.staminaPotsCount > 0)
         {
-            staminaPotsCount--;
+            inventoryScript.staminaPotsCount--;
             playerScript.currentStamina += 60;
-            itemAudioSource4.PlayOneShot(usePotSound);
-            staminaPotsText.text = staminaPotsCount + "";
+            itemAudioSource3.PlayOneShot(usePotSound);
+            staminaPotsText.text = inventoryScript.staminaPotsCount.ToString();
         }
     }
 
@@ -269,7 +181,7 @@ public class TreatmentUI : MonoBehaviour {
     public void HoverButton()
     {
 
-        itemAudioSource3.PlayOneShot(itemDesciptionSound);
+        itemAudioSource2.PlayOneShot(itemDesciptionSound);
 
     }
 
