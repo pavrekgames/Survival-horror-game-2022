5f8e5d8edf03bbacc5d2ed665cb53b2f7dffa1d8
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-12-06 12:35:38 +0100

    Refactored scripts

--- Notifications/NotificationUI.cs
+++ Notifications/NotificationUI.cs
@@ -1,15 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class NotificationUI : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public TextMeshProUGUI mainNotificationTextMesh;
+    public TextMeshProUGUI infoNotificationTextMesh;
+    public TextMeshProUGUI taskNotificationTextMesh;
+    public TextMeshProUGUI doorNotificationTextMesh;
+    public TextMeshProUGUI saveGameNotificationTextMesh;
+    public TextMeshProUGUI collectibleNotificationTextMesh;
+    public TextMeshProUGUI secretPlacesNotificationTextMesh;
+
+    public Canvas herbsNotificationCanvas;
+    public Canvas saveGameNotificationCanvas;
+    public Canvas pushNotificationCanvas;
+    public Canvas secretNotificationCanvas;
+    public Canvas itemsNotificationCanvas;
+    public Canvas inventoryNotificationCanvas;
+
+    public string lightHint = "Press <color=#280DF6FF> Middle Mouse Button</color> to increase range of light";
+    public string tasksHint = "Press <color=#280DF6FF>[ I ]</color> to see Inventory and Tasks";
+    public string sprintHint = "Press <color=#280DF6FF>[Left Shift]</color> to sprint";
+    public string mapHint = "Press <color=#280DF6FF>[M]</color> to see a Map";
+    public string crouchHint = "Press <color=#280DF6FF>[C]</color> to Crouch";
+    public string doorHint = "Hold down <color=#280DF6FF>[Left Mouse Button]</color> and move the mouse in order to move the door.";
+
+    public string secretItemNotification = "You picked up a Secret Item";
+    public string greenHerbNotification = "You picked up a <color=#33D047FF>Green Herb</color>";
+    public string blueHerbNotification = "You picked up a <color=#006EFFFF>Blue Herb</color>";
+    public string vialNotification = "You picked up a <color=#F6FF04FF>Vial</color>";
+    public string staminaPotNotification = "You picked up a <color=#FF00FFFF>Stamina Mixture</color>";
+    public string healthPotNotification = "You picked up a <color=#F6FF04FF>Health Potion</color>";
+    public string badgeNotification = "You picked up a <color=#F6FF04FF>Badge</color> to collection";
+    public string photoNotification = "You picked up a <color=#F6FF04FF>Photo</color> to collection";
+    public string tipNotification = "You picked up a <color=#F6FF04FF>Tip</color> to collection";
+
+    void Start () {
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
 		
 	}
