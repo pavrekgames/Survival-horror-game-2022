228e50499c4db95f2d1874319297c221cca3a5b3
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-12-11 18:18:05 +0100

    Refactored Inventory scripts

--- Inventory/TaskItem.cs
+++ Inventory/TaskItem.cs
@@ -19,6 +19,7 @@ public class TaskItem : MonoBehaviour, IItem {
 
     public void PickUpItem()
     {
+        item.Reset();
         inventoryScript.AddItem(item, itemSound);
         gameObject.SetActive(false);
 
