d7f6491786206efbb4fb88a06520805d433e6744
pavrekgames <105421661+pavrekgames@users.noreply.github.com>
2022-12-13 12:29:10 +0100

    Refactored Notifications scripts

--- Notifications/Notifications.cs
+++ Notifications/Notifications.cs
@@ -12,33 +12,32 @@ public class Notifications : MonoBehaviour {
 	private Camera playerCam;
 	[SerializeField] private float rayLength = 4f;
 
-    void OnEnable () {
+    void OnEnable()
+    {
 
         playerManagerScript = GameObject.Find("Player").GetComponent<PlayerManager>();
         playerCam = Camera.main;
 
     }
 
-	void Update () {
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && playerManagerScript.isPlayerCanInput == true)
+        {
+            Ray playerAim = playerCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
 
-		if (Input.GetMouseButtonDown (0) && playerManagerScript.isPlayerCanInput == true) {
-
-			Ray playerAim = playerCam.ViewportPointToRay (new Vector3 (0.5f, 0.5f, 0));
-			RaycastHit hit; 
-
-			if (Physics.Raycast (playerAim, out hit, rayLength, 1 << 9)) {
-
-				if (hit.collider.gameObject.tag == "Door" || hit.collider.gameObject.tag == "Hand") {
-
+            if (Physics.Raycast(playerAim, out hit, rayLength, 1 << 9))
+            {
+                if (hit.collider.gameObject.tag == "Door" || hit.collider.gameObject.tag == "Hand")
+                {
                     if (hit.transform.gameObject.GetComponent<RaycastNotification>())
                     {
                         hit.transform.gameObject.GetComponent<RaycastNotification>().SendNotification();
                     }
-				}
-			} 
-		} 
-} 
-
-	
+                }
+            }
+        }
+    }
 }
 
